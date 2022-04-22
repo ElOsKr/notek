7ca6902391371a3d016eeaebe68adfcfd464c1f3
davman15 <63202994+davman15@users.noreply.github.com>
2022-04-22 19:47:02 +0200

    Primer Commit
    Login esta en proceso

---
+++ js/firebase_Iniciar_Sesion.js
@@ -0,0 +1,19 @@
+function redirigir() {
+    location.replace("../html/perfil.html");
+}
+
+function iniciarSesion() {
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    if(email==""){
+       //Rellene el email
+    } 
+    else if(password==""){
+        //Rellene la contraseña
+    }
+    else{
+        //Entra correctamente al sitio que le corresponde en este caso el index.html    
+    }
+}
