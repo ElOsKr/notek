248389c8a5747a147ac777e02296c1416f625578
davman15 <63202994+davman15@users.noreply.github.com>
2022-04-26 21:04:08 +0200

    Animaciones añadidas en local

--- sql/consultas.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP (1000) [ID]
-      ,[Nombre]
-      ,[Apellidos]
-      ,[Correo]
-      ,[Contrasena]
-      ,[ID_Usuario]
-  FROM [Notek].[dbo].[Usuarios]
-  INSERT into Usuarios(Nombre,Apellidos,Correo,Contrasena,ID_Usuario) 
-  VALUES ('David','Manrique','davidmanrique15@gmail.com','cocaine39','davman15');
