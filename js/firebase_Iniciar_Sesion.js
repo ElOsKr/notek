20e74bd1638fe84ee9ad686a4eba71148e081696
davman15 <63202994+davman15@users.noreply.github.com>
2022-04-26 11:14:38 +0200

    Libreria sweetalert

--- js/firebase_Iniciar_Sesion.js
+++ js/firebase_Iniciar_Sesion.js
@@ -6,14 +6,20 @@ function iniciarSesion() {
 
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    
-    if(email==""){
-       //Rellene el email
-    } 
-    else if(password==""){
+
+    if (email == "") {
+        //Rellene el email
+        Swal.fire({
+            icon: 'error',
+            title: 'Introduzca sus credenciales',
+            text: 'Dejó el campo Contraseña vacío'
+        });
+
+    }
+    else if (password == "") {
         //Rellene la contraseña
     }
-    else{
+    else {
         //Entra correctamente al sitio que le corresponde en este caso el index.html    
     }
 }
