d08bf7ae8244f55be67dce2d86fa3669158ab262
davman15 <63202994+davman15@users.noreply.github.com>
2022-04-26 11:19:04 +0200

    codigo js

---
+++ js/passOlvidada.js
@@ -0,0 +1,8 @@
+document.addEventListener("readystatechange", cargarEventos, false);
+
+function cargarEventos() {
+
+    if (document.readyState == "interactive") {
+        //document.getElementsByTagName("button")[0].addEventListener("click", crear, false);
+    }
+}
