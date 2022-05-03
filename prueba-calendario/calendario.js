50276e76087d6b2ab6a62ef2746275b6bd84d000
ElOsKr <eloskr16@gmail.com>
2022-05-03 13:06:36 +0200

    mejoras calendario

--- prueba-calendario/calendario.js
+++ prueba-calendario/calendario.js
@@ -1,20 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('aniadirFechas').addEventListener('click',guardarEvento);
   var myModal = new bootstrap.Modal(document.getElementById('modalCalendario'))
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     locales: 'es',
     headerToolbar: {
-      left: "prev,sigu,hoy",
+      left: "prev,next,today",
       center: "title",
-      right: "dayGridMonth, timeGridWeek, listWeek"
-
+      right: "dayGridMonth timeGridWeek timeGridDay listWeek",
     },
+    buttonText:{
+      today:'Hoy',
+      month:'Mes',
+      week:'Semana',
+      day: 'Dia',
+      list:'Lista'
+    } , 
     dateClick: function (datos) {
       document.getElementById("fechaInicio").value = datos.dateStr;
+      document.getElementById("fechaFin").value = datos.dateStr;
       myModal.show();
 
     }
   });
   calendar.render();
+
+
 });
+
+
+
+
+function guardarEvento(){
+  var tituloEvento=document.getElementById('nombreEvento').value;
+  var fechaInicio=document.getElementById('fechaInicio').value;
+  var fechaFin=document.getElementById('fechaFin').value;
+  var colorEvento=document.getElementById('colorEvento').value;
+  var datos=[tituloEvento,fechaInicio,fechaFin,colorEvento];
+  var cadenaDatos=JSON.stringify(datos);
+  var cadena='x='+cadenaDatos;
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    if(this.readyState==4 && this.status==200){
+      var comprobar=JSON.parse(this.responseText);
+      if(comprobar==1){
+        alert ("Correcto")
+      }else{
+        alert("Error")
+      }
+    }
+  }
+  xhttp.open("POST", "calendario.php");
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(cadena);
+}
---
+++ prueba-calendario/calendario.php
@@ -0,0 +1,38 @@
+<?php
+    class conexion{
+
+        private $conn=null;
+        public function __construct()
+        {
+            $dsn="mysql:host=localhost;charset=utf8;";
+            $this->conn=new PDO($dsn,'root','');
+        }
+
+        public function agregarFechas($titulo,$fechaIni,$fechaFin,$color){
+            $datos=array(':titulo'=>$titulo,':fechaIni'=>$fechaIni,':fechaFin'=>$fechaFin,':color'=>$color);
+            $sql='insert into calendario.fechas values (:titulo,:fechaIni,:fechaFin,:color)';
+            $q=$this->conn->prepare($sql);
+            $q->execute($datos);
+            return $q;
+        }
+    }
+
+    $conexion=new conexion();
+
+
+    if(isset($_POST['x'])){
+        $datos=json_decode($_POST['x']);
+
+        if($conexion->agregarFechas($datos[0],$datos[1],$datos[2],$datos[3])){
+            echo json_encode(1);
+        }else{
+            echo json_encode(0);
+        }
+
+    }
+
+
+
+
+
+?>
