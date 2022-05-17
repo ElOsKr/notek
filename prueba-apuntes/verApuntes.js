9927a2e4fda5baf85354e3e3f2ca0232927c6bf3
ElOsKr <eloskr16@gmail.com>
2022-05-17 11:07:21 +0200

    ahora si terminado los apuntes

--- prueba-apuntes/verApuntes.js
+++ prueba-apuntes/verApuntes.js
@@ -16,16 +16,18 @@ function cargarEventos() {
       xhttp.onload = function() {
         if(this.readyState==4 && this.status==200){
           var comprobar=JSON.parse(this.responseText);
-          if(comprobar[0]==1){
+          if(comprobar[1]!=false){
             document.getElementById("tituloApuntes").innerHTML=comprobar[1]['title'];
             document.getElementById('contenidoApuntes').innerHTML=comprobar[1]['content'];
           }else{
-            alert("Error");
+            location.href="./inicioApuntes.php";
           }
         }
       }
       xhttp.open("POST", "comprobarApuntes.php");
       xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       xhttp.send(cadena); 
+    }else{
+      location.href="./inicioApuntes.php";
     }
   }
