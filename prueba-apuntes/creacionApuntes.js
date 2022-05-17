e33a53bf22039adb1eb66c0ba0ed7528acf37183
ElOsKr <eloskr16@gmail.com>
2022-05-17 10:57:18 +0200

    apuntes 100% terminados de verdad siuuuuuuuuuu

--- prueba-apuntes/creacionApuntes.js
+++ prueba-apuntes/creacionApuntes.js
@@ -2,7 +2,7 @@ document.addEventListener("readystatechange", cargarEventos, false);
 const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 function cargarEventos() {
   obtenerApuntes();
-  document.getElementById("botonVer").addEventListener("click",function(){location.href="./verApuntes.php?id="+document.getElementById("idApuntes").value})
+  document.getElementById("botonVer").addEventListener("click",verApuntes);
   document.getElementById("regresarApuntes").addEventListener("click",function(){location.href="./inicioApuntes.php"})
   document.getElementById("botonGuardar").addEventListener("click",guardarApuntes);
   tinymce.init({
@@ -13,6 +13,12 @@ function cargarEventos() {
       content_css: useDarkMode ? 'dark' : 'default',
   });
 }
+
+function verApuntes(){
+  guardarApuntes();
+  location.href="./verApuntes.php?id="+document.getElementById("idApuntes").value
+}
+
 
 function obtenerApuntes(){
   var id=document.getElementById("idApuntes").value;
@@ -28,6 +34,7 @@ function obtenerApuntes(){
         if(comprobar[0]==1){
           document.getElementById("tituloApuntes").value=comprobar[1]['title'];
           tinymce.get("default-editor").setContent(comprobar[1]['content']);
+          document.getElementById("botonVer").disabled=false;
         }else{
           alert("Error");
         }
@@ -55,7 +62,8 @@ function guardarApuntes(){
       var comprobar=JSON.parse(this.responseText);
       if(isNaN(comprobar)==false){
         document.getElementById("idApuntes").value=comprobar
-        alert("Todo bien pa")
+        document.getElementById("botonVer").disabled=false;
+        location.href='./apuntes.php?id='+comprobar;
       }else if(comprobar=="error"){
         alert("Error");
       }
