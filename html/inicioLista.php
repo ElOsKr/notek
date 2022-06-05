d9f0c1b1482bd80158b87f2ec014c1aceb0bc14a
ElOsKr <eloskr16@gmail.com>
2022-06-05 11:01:40 +0200

    mas ordenado la lista

--- html/inicioLista.php
+++ html/inicioLista.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista</title>
-    <script type="module" src="../js/inicioLista.js"></script>
-    <script src="../js/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="../css/inicioLista.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/sidebar.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <script type="module" src="../js/firebase.js"></script>
     <script type="module" src="../js/sidebar.js"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script type="module" src="../js/inicioLista.js"></script>
+    <script src="../js/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <div class="page">
