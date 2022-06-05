a8e3d166e23b0c75b124640b2f34572f7835b865
ElOsKr <eloskr16@gmail.com>
2022-06-05 20:55:40 +0200

    quitado actualizar fechas al ver apuntes

--- js/verApuntes.js
+++ js/verApuntes.js
@@ -31,11 +31,6 @@ pdf.save(document.getElementById("tituloApuntes").innerHTML+'.pdf');
 
 async function obtenerApunte() {
   let idApunte = localStorage.getItem("idApunte");
-  //Actualizo la fecha
-  const referenciaApunte = doc(db, "Usuarios",localStorage.getItem("id"),'Apuntes', localStorage.getItem("idApunte"));
-  await updateDoc(referenciaApunte, {
-    fechaApunte: Date.now()
-  });
   const unsub = onSnapshot(doc(db, "Usuarios",localStorage.getItem("id"),'Apuntes', idApunte), (doc) => {
     inputTituloApunte.innerHTML = doc.data().titulo;
     document.getElementById('contenidoApuntes').innerHTML += doc.data().contenido;
